143f16b3cdc3259e781048fc46a5b69c7acbc51e
Kri <kri.boats@gmail.com>
2026-03-30 13:03:54 +0200

    add transition to main menu

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,34 +1,40 @@
 class_name MainMenu
-extends CenterContainer
+extends Control
 
 @onready var continue_button: Button = %ContinueButton
-@onready var body: Control = $MarginContainer/VBoxContainer
-@onready var settings_menu: Control = $MarginContainer/SettingsMenu
+@onready var menu_body: Control = $MainMenu
+@onready var settings_menu: Control = $SettingsMenu
+@onready var transition: Control = $Transition
+@onready var transition_ap: AnimationPlayer = $Transition/AnimationPlayer
 
 
 func _ready():
-	continue_button.visible = SaveSystem.has_save_data()
-	body.show()
+	continue_button.disabled = !SaveSystem.has_save_data()
+	menu_body.show()
 	settings_menu.hide()
 
 
 func _on_new_game_button_pressed():
 	StageLoader.load_stage(0, 0)
+	await play_transition()
+
 	hide()
 
 
 func _on_continue_button_pressed():
-	await SaveSystem.load_game()
-
+	SaveSystem.load_game()
 	var world_idx: int = SaveSystem.get_property("world")
 	var stage_idx: int = SaveSystem.get_property("stage")
 	print("Loaded save data: world=" + str(world_idx) + ", stage=" + str(stage_idx))
+	
 	StageLoader.load_stage(stage_idx, world_idx)
+	await play_transition()
+	
 	hide()
 
 
 func _on_settings_button_pressed():
-	body.hide()
+	menu_body.hide()
 	settings_menu.show()
 
 
@@ -38,4 +44,14 @@ func _on_exit_button_pressed():
 
 func _on_back_button_pressed():
 	settings_menu.hide()
-	body.show()
+	menu_body.show()
+
+
+func play_transition():
+	StageLoader.stage.active = false
+	transition.show()
+	transition_ap.play("transition")
+	await transition_ap.animation_finished
+
+	transition.hide()
+	StageLoader.stage.active = true
